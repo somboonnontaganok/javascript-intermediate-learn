7559cb693d44a846c77588f9bdee41e63af4102e
somboonnontaganok <somboonnontaganok@gmail.com>
2023-03-06 22:20:59 +0700

    add result from  app.js to index.html

---
+++ app.js
@@ -0,0 +1,4 @@
+const lotto_number = prompt("Please select your number!");
+// console.log(lotto_number);
+// console.log(Math.floor(Math.random() *100));
+document.getElementById("result").innerHTML =  Math.floor(Math.random() *100);
