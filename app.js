d93cfa1dfe6cfe04e336109f08b4c7c9dd1787cd
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-07 23:22:10 +0700

    grade selection

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-let lotto_number = prompt("Please fill your number : ");
-let random_number = Math.floor(Math.random() * 3);
+let score = prompt("Please fill your score : ");
+// let random_number = Math.floor(Math.random() * 3);
 // document.getElementById("selected").innerHTML = lotto_number
-document.getElementById("random").innerHTML = random_number;
-if(lotto_number == random_number) {
-    document.getElementById("result").innerHTML = "Congratulations you win 3last number";
+if(score >= 80){
+    document.getElementById("result").innerHTML = "Your grade is A";
+}
+else if(score >= 70){
+    document.getElementById("result").innerHTML = "Your grade is B";
+}
+else if(score >= 60) {
+    document.getElementById("result").innerHTML = "Your grade is C";
+}
+else if(score >= 50){
+    document.getElementById("result").innerHTML = "Your grade is D";
 }
 else {
-    document.getElementById("result").innerHTML = "Sorry, you lost 3last number";
+    document.getElementById("result").innerHTML = "Your grade is F";
 }
