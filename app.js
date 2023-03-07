295c2cfd8a9cd5634972ce18793e0a917417228f
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-07 23:36:45 +0700

    use funciton sayHello

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-let score = prompt("Please fill your score : ");
-// let random_number = Math.floor(Math.random() * 3);
-// document.getElementById("selected").innerHTML = lotto_number
-if(score >= 80){
-    document.getElementById("result").innerHTML = "Your grade is A";
-}
-else if(score >= 70){
-    document.getElementById("result").innerHTML = "Your grade is B";
-}
-else if(score >= 60) {
-    document.getElementById("result").innerHTML = "Your grade is C";
-}
-else if(score >= 50){
-    document.getElementById("result").innerHTML = "Your grade is D";
-}
-else {
-    document.getElementById("result").innerHTML = "Your grade is F";
+function sayHello() {
+    alert('Hello');
 }
