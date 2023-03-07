5146cfd52f3da46adb277073d5062941879cacd7
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-07 20:50:27 +0700

    add result true and false for lottery

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const lotto_number = prompt("Please select your number!");
-// console.log(lotto_number);
-// console.log(Math.floor(Math.random() *100));
-document.getElementById("selected").innerHTML = lotto_number
-document.getElementById("result").innerHTML =  Math.floor(Math.random() *100);
+let lotto_number = prompt("Please fill your number : ");
+let random_number = Math.floor(Math.random() * 1000);
+// document.getElementById("selected").innerHTML = lotto_number
+document.getElementById("random").innerHTML = random_number;
+if(lotto_number == random_number) {
+    document.getElementById("result").innerHTML = "Congratulations you win 3last number";
+}
+else {
+    document.getElementById("result").innerHTML = "Sorry, you lost 3last number";
+}
