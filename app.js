7f33a0d71ec21b9f49d5917eea724619257497ba
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-07 08:50:40 +0700

    adjust bootstrap

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const lotto_number = prompt("Please select your number!");
 // console.log(lotto_number);
 // console.log(Math.floor(Math.random() *100));
+document.getElementById("selected").innerHTML = lotto_number
 document.getElementById("result").innerHTML =  Math.floor(Math.random() *100);
